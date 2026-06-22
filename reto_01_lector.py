c7f7166e1ce65920a51bdb09d0c100438f54bd37
danny <danny@example.com>
2026-06-22 16:44:24 -0600

    Crear plantilla para el reto 01

---
+++ reto_01_lector.py
@@ -0,0 +1,9 @@
+# Reto 01: Lector de Datos Basico
+# Objetivo: Aprender a leer un archivo de texto y contar sus lineas
+
+def contar_lineas(nombre_archivo):
+    # Aqui programaremos la logica mañana
+    pass
+
+if __name__ == "__main__":
+    print("Preparado para el Reto 01")
