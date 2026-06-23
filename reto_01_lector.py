a574ebc953ba6cecadca581d23a2ebe2745f53f0
danny <danny@example.com>
2026-06-23 15:19:06 -0600

    Completar Reto 01 lector de lineas limpio

--- reto_01_lector.py
+++ reto_01_lector.py
@@ -1,9 +1,19 @@
 # Reto 01: Lector de Datos Basico
 # Objetivo: Aprender a leer un archivo de texto y contar sus lineas
 
-def contar_lineas(nombre_archivo):
-    # Aqui programaremos la logica mañana
-    pass
+def contar_lineas(nombre_del_archivo):
+    contador = 0
+    #leer el texto
+    with open(nombre_del_archivo, 'r') as archivo:
+        for linea in archivo:
+            contador += 1
+            print(linea.strip())
+        print(f'Total de lineas en el archivo {contador}')
 
 if __name__ == "__main__":
-    print("Preparado para el Reto 01")
+    archivo = "datos.txt"
+    print('---Inicializando programa---')
+    contar_lineas(archivo)
+
+    
+    
