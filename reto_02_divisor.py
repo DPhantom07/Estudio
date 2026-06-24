d31ed883077647ab5755d8f348fcf8fdbcbe9eff
DPhantom <danny@example.com>
2026-06-24 10:46:11 -0600

    feat: implementar divisor de datos

--- reto_02_divisor.py
+++ reto_02_divisor.py
@@ -2,13 +2,14 @@
 # Objetivo: Leer un archivo CSV y separar sus columnas con .split()
 
 def procesar_usuarios(nombre_archivo):
+    #abrir el archivo y leer y darle el alias archivo
     with open(nombre_archivo, "r") as archivo:
+        #iterar el numero de linea que tiene archivo y guardar en linea
         for linea in archivo:
-            # 1. Limpiar la línea con .strip()
-            linea_limpia = linea.strip()
-            
-            # 2. Mañana romperemos la línea aquí usando .split(",")
-            print(linea_limpia)
+            #lista_limpia = linea sin espacios y separando la lista en cada ,
+            linea_limpia = linea.strip().split(',')
+            #imprime los primeros dos datos de la lista que esta en linea_limpia
+            print(f'{linea_limpia[0]}-{linea_limpia[1]}')
 
 if __name__ == "__main__":
     archivo_csv = "datos_estructurados.txt"
